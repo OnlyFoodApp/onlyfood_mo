8986ae056ca498d384255505770c9eb3b097b809
cubi-dev <cubiiwork@gmail.com>
2023-10-24 02:43:45 +0700

    [MENU]Add Menu For Campaign #1

--- onlyfood_mo/lib/main.dart
+++ onlyfood_mo/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:onlyfood_mo/responsive/mobile_screen_layout.dart';
 import 'package:onlyfood_mo/responsive/responsive_layout_screen.dart';
 import 'package:onlyfood_mo/responsive/web_screen_layout.dart';
 import 'package:onlyfood_mo/screens/campaign/add_new_campaign.dart';
+import 'package:onlyfood_mo/screens/campaign/add_menu_for_campaign.dart';
 import 'package:onlyfood_mo/screens/login/login_screen.dart';
 import 'package:onlyfood_mo/screens/profile_editing/profile_editing.dart';
 import 'package:onlyfood_mo/screens/reset_password/reset_password.dart';
@@ -48,6 +49,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor,
         ),
-        home: const AddNewCampaign());
+        home: const AddMenuForCampaign());
   }
 }
